b5418462902589a598a1da497a9193b99c917f62
NIk134 <92600545+Nik134570@users.noreply.github.com>
2022-02-18 18:57:15 +0400

    Add files via upload

---
+++ pythonProject7/main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("base.html", title ="заголовок")
+
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template("training.html", prof=prof)
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
